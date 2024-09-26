d19f9aec6216e2318cfcddc1eeaecc5e8b1c2c33
CamiloCastrillon <103201304+CamiloCastrillon@users.noreply.github.com>
2024-09-26 16:07:11 -0500

    .
    .

--- ili2any/deploy_ili2any.py
+++ ili2any/deploy_ili2any.py
@@ -1,4 +1,4 @@
-from utils.ili2any import ili2gpkg
+from ili2any import ili2gpkg
 
 ili_path    = 'C:\camilo\Monitoria\monitoria_harold\info_geografica\ili\Submodelo_Cartografia_Catastral_V1_2.ili'
 gpkg_path   = 'C:\camilo\Monitoria\monitoria_harold\info_geografica\ili\Submodelo_Cartografia_Catastral_V1_2.gpkg'
